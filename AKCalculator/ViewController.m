5d4d10b0e69f4f3b56322d138ce93121906c284f
Rose <rose@Roses-Mac.local>
2017-05-14 17:53:35 -0700

    Comments to actions have been added, some code style has been made

--- AKCalculator/ViewController.m
+++ AKCalculator/ViewController.m
@@ -17,13 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UISwipeGestureRecognizer *swipeRecognizer = [[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeRecognizer:)] autorelease];
+    UISwipeGestureRecognizer *swipeRecognizer = [[[UISwipeGestureRecognizer alloc] initWithTarget:self
+                                                                                           action:@selector
+                                                  (handleSwipeRecognizer:)] autorelease];
     [self.displayLabel addGestureRecognizer:swipeRecognizer];
 }
 
+/* 
+ cleans the last symbol in displayLabelText;
+ sets the value to @"0" if all symbols were removed
+ */
 - (void)handleSwipeRecognizer:(UISwipeGestureRecognizer *)swipeRecognizer {
     NSString *displayLabelText = self.displayLabel.text;
     NSString *result = [displayLabelText substringToIndex:self.displayLabel.text.length - 1];
+    
     if (result.length == 0) {
         self.displayLabel.text = @"0";
     } else {
@@ -31,11 +38,15 @@
     }
 }
 
+/*
+ digitButtons handler
+ formats string as float value (to prevent entering leading zero)
+ if the displayLabelText contains "." dot-symbol
+ or appends a new digit to the displayLabelText if it does not
+ */
 - (IBAction)digitTapped:(id)sender {
     NSString *digit = [sender titleForState:UIControlStateNormal];
-    
     NSString *result = [NSString stringWithFormat:@"%@%@", self.displayLabel.text, digit];
-    
     NSString *displayLabelText = self.displayLabel.text;
     
     if ([displayLabelText rangeOfString:@"."].location == NSNotFound) {
@@ -45,13 +56,17 @@
     }
 }
 
+// reset displayLabelText value to @"0"
 - (IBAction)clearButtonTapped:(id)sender {
     self.displayLabel.text = @"0";
 }
 
+/*
+ dotButton handler
+ appends a dot to the displayLabelText only once
+ */
 - (IBAction)dotButtonTapped:(id)sender {
     NSString *dot = [sender titleForState:UIControlStateNormal];
-    
     NSString *displayLabelText = self.displayLabel.text;
     
     if ([displayLabelText rangeOfString:@"."].location == NSNotFound) {
@@ -73,4 +88,5 @@
     [_digitCollectionButtons release];
     [super dealloc];
 }
+
 @end
